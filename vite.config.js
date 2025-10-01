ed7b03f6d7536d785bf034392c0fb39797a4e8d4
Shaik-Nazidulla <nazidullashaik@gmail.com>
2025-10-01 18:16:25 +0530

    test2vercelconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-// https://vite.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: './', 
+  build: {
+    outDir: 'dist' 
+  }
 })
